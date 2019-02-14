f66dddd8982c7fae70fa8e2ee87d02d86fde7b5c
udhos <udhos>
2019-02-14 17:07:46 +0000

    Sketch.

---
+++ spotbq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+msg() {
+	echo 1>&2 $0: $*
+}
+
+die() {
+	msg $*
+	exit 1
+}
+
+tmp=/tmp/eraseme
+
+id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
+[[ $id = i-* ]] || msg bad instanceid=$id
+
+msg instanceid=$id
+
+aws ec2 describe-instances --region sa-east-1 --instance-ids $id > $tmp || msg describe-instances failure
+
+lifecycle=$(jq -r '.Reservations[0].Instances[0].InstanceLifecycle' < $tmp)
+
+msg lifecycle=$lifecycle
