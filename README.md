61eb921eb00eed4ccfeaf05e4b052a0bd5bc6008
udhos <udhos>
2019-02-14 21:56:34 +0000

    bq load.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # spotbq
-spotbq
+
+List datasets:
+
+    bq ls --project_id PROJECT_ID
+
+Create dataset:
+
+    bq mk --dataset PROJECT_ID:DATASET
+
+List tables:
+
+    bq ls PROJECT_ID:DATASET
+
+Create table:
+
+    bq mk --table PROJECT_ID:DATASET.TABLE schema.json
+
+Show table:
+
+    bq show PROJECT_ID:DATASET.TABLE
+
+Show head:
+
+    bq head --max_rows=10 PROJECT_ID:DATASET.TABLE
+
+Delete table:
+
+    bq rm -t PROJECT_ID:DATASET.TABLE
+
+Query:
+
+    bq query --nouse_legacy_sql 'select * from `PROJECT_ID.DATASET.TABLE` limit 100'
+
+Insert:
+
+    bq load --source_format=CSV PROJECT_ID:DATASET.TABLE data.csv schema.json
+
+Delete data:
+
+    bq query --nouse_legacy_sql 'delete from `PROJECT_ID.DATASET.TABLE` where true'
+
--- spotbq.sh
+++ spotbq.sh
@@ -9,6 +9,16 @@ die() {
 	exit 1
 }
 
+cat <<__EOF__
+PROJECT_ID=$PROJECT_ID
+DATASET=$DATASET
+TABLE=$TABLE
+__EOF__
+
+[ -z "$PROJECT_ID" ] && die missing PROJECT_ID
+[ -z "$DATASET" ] && die missing DATASET
+[ -z "$TABLE" ] && die missing TABLE
+
 tmp=/tmp/eraseme
 
 id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id || curl meta-data failure)
@@ -22,3 +32,23 @@ aws ec2 describe-instances --region sa-east-1 --instance-ids $id > $tmp || msg d
 lifecycle=$(jq -r '.Reservations[0].Instances[0].InstanceLifecycle' < $tmp || msg jq lifecycle failure)
 
 msg lifecycle=$lifecycle
+
+if [ "$lifecycle" != spot ]; then
+	lifecycle=non-spot
+fi
+
+upload() {
+	local m="$*"
+	# YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S
+	now=$(date +'%Y-%m-%d %H:%M:%S') 
+	printf "\"$now\",\"$id\",\"$lifecycle\",\"$m\"\n" > data.csv
+	cat data.csv
+	bq load --source_format=CSV $PROJECT_ID:$DATASET.$TABLE data.csv schema.json
+}
+
+upload boot
+
+while :; do
+	sleep 5
+	upload ping
+done
