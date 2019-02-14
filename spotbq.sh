5e603c387d496de2f0543e66e69a684a1e987d55
udhos <udhos>
2019-02-14 17:12:19 +0000

    Report error.

--- spotbq.sh
+++ spotbq.sh
@@ -11,7 +11,7 @@ die() {
 
 tmp=/tmp/eraseme
 
-id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+id=$(curl http://169.254.169.254/latest/meta-data/instance-id || curl meta-data failure)
 
 [[ $id = i-* ]] || msg bad instanceid=$id
 
@@ -19,6 +19,6 @@ msg instanceid=$id
 
 aws ec2 describe-instances --region sa-east-1 --instance-ids $id > $tmp || msg describe-instances failure
 
-lifecycle=$(jq -r '.Reservations[0].Instances[0].InstanceLifecycle' < $tmp)
+lifecycle=$(jq -r '.Reservations[0].Instances[0].InstanceLifecycle' < $tmp || msg jq lifecycle failure)
 
 msg lifecycle=$lifecycle
